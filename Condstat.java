cdaa7bd5010777a242bd122bea9803a1d7da7392
Elango Kannan <elango.kiot@gmail.com>
2025-01-17 21:24:26 +0530

    Conditional Statement.

---
+++ Condstat.java
@@ -0,0 +1,14 @@
+// import java.io.*;
+public class Condstat
+{
+public static void main(String[] args)
+{
+int a=99;
+if( a>50 ){
+System.out.println("YES");
+}
+else{
+System.out.println("NO");
+}
+}
+}
