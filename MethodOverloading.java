57378fb67d8692df3d4c31eed3489e4f522cadba
Elango Kannan <elango.kiot@gmail.com>
2025-01-24 21:13:30 +0530

    Method overloading in Java.

---
+++ MethodOverloading.java
@@ -0,0 +1,21 @@
+public class MethodOverloading 
+{
+    static double myMethod(double x, double t) 
+    {
+        return x + t;
+    }
+
+    static int myMethod(int x, int y) 
+    {
+        return x + y;
+    }
+
+    public static void main(String[] args) 
+    {
+        int c = myMethod(1, 3);
+        System.out.println("The Sum is: " + c);
+        double d = myMethod(99.99, 99.99);
+        System.out.println("The Sum is: " + d);
+    }
+}
+ 
